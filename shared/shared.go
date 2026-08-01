015daad92ca15397919a0dadc175c11852c5a47c
lilybw <lilybwanscher@gmail.com>
2026-08-01 22:15:17 +0200

    RouterLike adapter

--- shared/shared.go
+++ shared/shared.go
@@ -1,6 +1,8 @@
 package shared
 
-import "net/http"
+import (
+	"net/http"
+)
 
 type HMRConfig struct {
 	// Disabled turns HMR off even when a config is supplied
@@ -9,10 +11,31 @@ type HMRConfig struct {
 	// injected client connects. Defaults to "/__go_solid_hmr__".
 	HMRPath string
 	// ServeMux, Router or the like to mount the WebSocket handler on. Required when HMR is enabled.
+	// Adapters are available for:
+	// 	github.com/gorilla/mux.Router use MuxLikeFromRouterLike
 	Mux MuxLike
 }
 
 // MuxLike is anything that can register an http.Handler under a string pattern
 type MuxLike interface {
 	Handle(pattern string, handler http.Handler)
+}
+
+type RouterLike[T any] interface {
+	Handle(pattern string, handler http.Handler) T
+}
+
+// Sometimes I get a tired of go's type system...
+type functionToMethodWrapper[T func(pattern string, handler http.Handler)] struct{ fn T }
+
+func (i *functionToMethodWrapper[T]) Handle(pattern string, handler http.Handler) {
+	i.fn(pattern, handler)
+}
+
+func MuxLikeFromRouterLike(router RouterLike[any]) MuxLike {
+	return &functionToMethodWrapper[func(pattern string, handler http.Handler)]{
+		fn: func(pattern string, handler http.Handler) {
+			router.Handle(pattern, handler)
+		},
+	}
 }
